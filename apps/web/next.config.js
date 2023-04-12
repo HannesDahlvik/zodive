6a1ad775dc6f2e78272737dabcece79b3bb4d06b
HannesDahlvik <hannes.dahlvik@gmail.com>
2023-04-12 22:52:01 +0300

    .env checker

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,11 +1,15 @@
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+const { env } = require('@zodive/env')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         appDir: true
     },
-    transpilePackages: ['@zodive/api', '@zodive/auth', '@zodive/db', '@zodive/ui'],
+    publicRuntimeConfig: {
+        NODE_ENV: env.NODE_ENV
+    },
+    transpilePackages: ['@zodive/api', '@zodive/auth', '@zodive/db', '@zodive/env', '@zodive/ui'],
     webpack: (config, { isServer }) => {
         if (isServer) {
             config.plugins = [...config.plugins, new PrismaPlugin()]
---
+++ packages/env/index.js
@@ -0,0 +1,20 @@
+const { z } = require('zod')
+
+const envSchema = z.object({
+    MYSQL_PASSWORD: z.string(),
+    DATABASE_URL: z.string().url(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    NODE_ENV: z.enum(['development', 'production'])
+})
+
+const env = envSchema.safeParse(process.env)
+
+if (!env.success) {
+    console.error('Invalid environment variables:', JSON.stringify(env.error.format(), null, 4))
+    process.exit(1)
+}
+
+module.exports.env = env.data
